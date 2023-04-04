1368f874aa2775f8ef3428ebaacfe6a8b49fff30
boni <inkey.cherry@gmail.com>
2023-04-04 21:50:02 +0300

    CPack - 1

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -1,7 +1,5 @@
-# Инклюдим необходимые библиотеки
 include(InstallRequiredSystemLibraries)
 
-#Задаём кучу переменных
 set(CPACK_PACKAGE_CONTACT inkey.cherry@yandex.ru)
 set(CPACK_PACKAGE_VERSION ${PRINT_VERSION})
 set(CPACK_PACKAGE_DESCRIPTION_FILE ${CMAKE_CURRENT_SOURCE_DIR}/DESCRIPTION)
@@ -9,10 +7,8 @@ set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "C++ app for solving quadratic equations")
 set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE)
 set(CPACK_RESOURCE_FILE_README ${CMAKE_CURRENT_SOURCE_DIR}/README.md)
 
-# Говорим что игнорить
 set(CPACK_SOURCE_IGNORE_FILES  "\\\\.cmake;/build/;/.git/;/.github/" )
 
-# Говорим что и как инсталлить в архивник с сурсами
 set(CPACK_SOURCE_INSTALLED_DIRECTORIES "${CMAKE_SOURCE_DIR}; /")
 
 set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
@@ -22,7 +18,6 @@ set(CPACK_DEBIAN_FILE_NAME "solver-${PRINT_VERSION}.deb")
 set(CPACK_DEBIAN_PACKAGE_VERSION ${PRINT_VERSION})
 set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "all")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "ledibonibell")
-#set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Solves quadratic equations")
 set(CPACK_DEBIAN_PACKAGE_RELEASE 1)
 
 set(CPACK_GENERATOR "DEB")
