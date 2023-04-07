d0eecd87d501a0eb1f04a6e42697f93359b84a93
Egor Devyatkin <125737299+ledibonibell@users.noreply.github.com>
2023-04-07 18:19:04 +0300

    Some fix

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -1,22 +1,15 @@
-# Инклюдим необходимые библиотеки
 include(InstallRequiredSystemLibraries)
 
-#Задаём кучу переменных
 set(CPACK_PACKAGE_CONTACT inkey.cherry@gmail.com)
 set(CPACK_PACKAGE_VERSION ${PRINT_VERSION})
+
 set(CPACK_PACKAGE_DESCRIPTION_FILE ${CMAKE_CURRENT_SOURCE_DIR}/DESCRIPTION)
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "C++ app for solving quadratic equations")
 set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE)
 set(CPACK_RESOURCE_FILE_README ${CMAKE_CURRENT_SOURCE_DIR}/README.md)
 
-# Говорим что игнорить
-set(CPACK_SOURCE_IGNORE_FILES 
-"\\\\.cmake;/build/;/.git/;/.github/"
-)
-
-# Говорим что и как инсталлить в архивник с сурсами
+set(CPACK_SOURCE_IGNORE_FILES  "\\\\.cmake;/build/;/.git/;/.github/")
 set(CPACK_SOURCE_INSTALLED_DIRECTORIES "${CMAKE_SOURCE_DIR}; /")
-
 set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
 
 set(CPACK_DEBIAN_PACKAGE_NAME "solverapp-dev")
